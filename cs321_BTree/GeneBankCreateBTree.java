65714f6d81a0e6213ccbc970a84be1d266c80432
Tim Wilson <timwilson139@u.boisestate.edu>
2017-04-16 20:47:05 -0600

    updating CreateBTree class

--- cs321_BTree/GeneBankCreateBTree.java
+++ cs321_BTree/GeneBankCreateBTree.java
@@ -51,10 +51,10 @@ public class GeneBankCreateBTree {
 	
 	public static String keyToString(long theKey) {
 		String theSequence = "";
-		for (int i = 0; i < 32; i += 2) {
+		for (int i = 0; i < 31; i += 1) {
 			//System.out.println(subsequence.substring(i, i+1));
 			//long base = mapBase(subsequence.substring(i, i+1));
-			long theBits = (theKey>>i) & (~(~0<<2));
+			long theBits = (theKey>>2*i) & (~(~0<<2));
 			//System.out.println("i: " + i + ", theBits: " + Long.toBinaryString(theBits));
 			String base = mapKey(theBits);
 			theSequence += base;
@@ -81,14 +81,52 @@ public class GeneBankCreateBTree {
 	
 	
 	public static void main(String args[]) {
-		String testSequence = "actgaactg";
-		long testKey = stringToKey(testSequence);
-		System.out.println("testKey: "+Long.toBinaryString(testKey));
-		long mapTest = mapBase("t");
-		//System.out.println("mapBase: "+mapTest);
-		String returnedSeq = keyToString(testKey);
-		System.out.println("returnedSeq: " + returnedSeq);
+		if (args.length < 3) {
+			printUsage();
+		}
 		
+		int inputType = Integer.parseInt(args[0]);
+		if(!(inputType == 1 || inputType == 2 || inputType == 3)) {
+			throw new IllegalArgumentException("Improper Degree Selection");
+		}
+		
+		//TODO: check gbk file
+		
+		int sequenceLength = Integer.parseInt(args[2]);
+		if(sequenceLength <= 0 ) {
+			throw new IllegalArgumentException("Improper Load Factor Specification");
+		}
+		
+		int debugMode = 0;
+		if (args.length > 3) {
+			debugMode = Integer.parseInt(args[3]);
+			if(!(debugMode == 0 || debugMode == 1)) {
+				throw new IllegalArgumentException("Improper Debug Mode Selection");
+			}
+		}
+		
+	}
+	
+	/**
+	 * prints the proper command line arguments if the wrong inputs are detected
+	 */
+	private static void printUsage() {
+		System.out.println(
+			"Usage:\n"
+			+ " java GeneBankCreateBTree <degree> <gbk file> <sequence length> [<debug level>]\n"
+			+ " degree is the degree to be used for the BTree. If 0 is entered, the program will\n"
+			+ " choose the optimum degree based on a disk block size of 4096 bytes and the size\n"
+			+ " of the BTreeNode on disk\n"
+			+ " gbk file is the file of DNA sequences\n"
+			+ " sequence length is the length of DNA bases to be stored in each BTreeNode\n"
+			+ " debug level [optional]:\n"
+			+ " 0 Any diagnostic messages, help and status messages must be be printed on standard\n"
+			+ " error stream.\n"
+			+ " 1 The program writes a text file named dump, that has the following line format:\n"
+			+ " <frequency> <DNA string>. The dump file contains frequency and DNA string\n"
+			+ "(corresponding to the key stored) in an inorder traversal.\n"
+		);
+		System.exit(1);
 	}
 	
 

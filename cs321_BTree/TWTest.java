48e0283eb13d88a67c9f591a7d719337464d0f51
Tim Wilson <timwilson139@u.boisestate.edu>
2017-04-16 20:46:24 -0600

    updating test class

--- cs321_BTree/TWTest.java
+++ cs321_BTree/TWTest.java
@@ -4,6 +4,13 @@ public class TWTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String testSequence = "actgaactg";
+		System.out.println("testSequence: "+testSequence);
+		long testKey = GeneBankCreateBTree.stringToKey(testSequence);
+		System.out.println("testKey: "+Long.toBinaryString(testKey));
+		String returnedSeq = GeneBankCreateBTree.keyToString(testKey);
+		System.out.println("returnedSeq: " + returnedSeq);
+		
 
 	}
 

304a8e40f81a41975d569e1172252587847c30d9
TylerNicholls <tylermnicholls@gmail.com>
2017-04-18 16:34:45 -0600

    Parser might be working

--- cs321_BTree/TWTest.java
+++ cs321_BTree/TWTest.java
@@ -1,8 +1,13 @@
-
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class TWTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		String testSequence = "actgaactg";
 		System.out.println("testSequence: "+testSequence);
@@ -12,6 +17,23 @@ public class TWTest {
 		System.out.println("returnedSeq: " + returnedSeq);
 		
 
+		//Tyler's test
+//		File file = new File("something.gbk");
+//		int sequenceLength = 3;
+//		Parser aparse = new Parser(file, 3);
+//		aparse.parse();
+		BTLinkedList list = new BTLinkedList(3);
+		FileInputStream test = new FileInputStream("test.txt");
+		DataInputStream data = new DataInputStream(test);
+		
+		while(data.available()>0){
+			list.add((char)data.readByte());
+		}
+		
+		while(!list.isEmpty()){
+			System.out.println(list.getSubsequence());
+			
+		}
 	}
 
 }

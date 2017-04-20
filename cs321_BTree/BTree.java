34b13168acde3d1555c3f6789a640c54ee352767
TylerNicholls <tylermnicholls@gmail.com>
2017-04-20 14:42:52 -0600

    less errors in the BTree

--- cs321_BTree/BTree.java
+++ cs321_BTree/BTree.java
@@ -4,12 +4,11 @@ public class BTree {
 	BTreeNode myRoot;
 	long fileOffset;
 	File btreeFile;
-	int t; 	//degree
-
+	
 	public BTree(){
-
+		
 	}
-
+	
 	public void insert(long sskey){			//doesn't need to return anything but we can if we want!
 		if(myRoot == null){
 			myRoot = new BTreeNode();
@@ -17,18 +16,20 @@ public class BTree {
 			myRoot.setFileOffset(fileOffset);
 			myRoot.writeNode();
 		}
-
+		
 		myRoot.insert(sskey);
-
+		
 	}
-
+	
 	public boolean find(long sskey){		//true if the tree contains the element, can also return frequency instead
-		//TODO
-		System.out.println("Implement BTree foind()");
+		if(myRoot.sskey == sskey){
+			return true;
+		}
+		
 		return false;
 	}
-
-
+	
+	
 	public BTreeNode createNode(){
 		BTreeNode aNode = new BTreeNode();
 		fileOffset = btreeFile.length();
@@ -36,53 +37,51 @@ public class BTree {
 		aNode.writeNode();
 		return aNode;
 	}
-
+	
 	public class BTreeNode{
+		private BTreeNode leftChild;
+		private BTreeNode rightChild;
+		private BTreeNode myparent;
+		private int t;
+		private long sskey;
+		
 		public BTreeNode(){				//constructor
-
 		}
 
-		private BTreeNode BTreeNode(long sskey) {
-			// TODO Auto-generated method stub
-			return null;
-		}
 
 		void insert(long sskey){
-			if(BTreeNode(sskey).numChildren() == 2*t-1){
+			if(numChildren() == 2*t-1){
 				//split
 			}
 			if(numChildren() == 0){
 				insert(sskey);			//leaf
 			}
 
-			if(myparent() != null){		//insertion sort #efficiency
+			if(myparent != null){		//insertion sort #efficiency
 				//split by adding 1 new Node
 				//push median to parent
-				BTreeNode splitNode = createNode();
+				splitNode = createNode();
 			}
 
 		}
-
-
-		private Object myparent() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		private int numChildren() {
-			// TODO Auto-generated method stub
+		public int numChildren(){
+			if (leftChild != null && rightChild !=null){
+				return 2;
+			}
+			else if (leftChild != null || rightChild != null){
+				return 1;
+			}
 			return 0;
 		}
-
-
+		
 		public void setFileOffset(long fileOffset) {
 			// TODO Auto-generated method stub
-
+			
 		}
-
+		
 		public void writeNode() {
 			// TODO Auto-generated method stub
-
+			
 		}
 	}
 
